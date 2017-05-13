efe21233dfb4a7ab14ba04d7b7149665ced0ba0c
Shahrukh <shahrukh.haider35@gmail.com>
2017-05-13 10:44:19 +0530

    added HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
